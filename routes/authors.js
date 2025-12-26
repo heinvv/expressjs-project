c8c45da5e81b142017473b828c040a992229344a
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2025-12-26 20:53:54 +0200

    Add search form

--- routes/authors.js
+++ routes/authors.js
@@ -1,16 +1,40 @@
 const express = require( 'express' );
 const router = express.Router();
+const Author = require('../models/author');
 
-router.get( '/', ( req, res ) => {
-    res.render( 'authors/index' );
+router.get( '/', async( req, res ) => {
+    let searchOptions = {};
+    if ( !! req?.query?.name ) {
+        searchOptions.name = new RegExp( req.query.name, 'i' );
+    }
+    try {
+        const authors = await Author.find(searchOptions);
+        res.render( 'authors/index', { 
+            authors: authors, 
+            searchOptions: req.query
+        } );
+    } catch {
+        res.redirect( '/' );
+    }
 });
 
 router.get( '/new', ( req, res ) => {
-    res.render( 'authors/new' );
+    res.render( 'authors/new', { author: new Author() } );
 });
 
-router.post( '/', ( req, res ) => {
-    res.send( 'create' );
+router.post( '/', async ( req, res ) => {
+    const author = new Author({
+        name: req.body.name
+    });
+    try {
+        const newAuthor = await author.save();
+        res.redirect( '/authors' );
+    } catch ( err ) {
+        res.render( 'authors/new',{ 
+            author: author, 
+            errorMessage: 'Error creating author'
+        } );
+    }
 });
 
 // router.get( '/:id', ( req, res ) => {
