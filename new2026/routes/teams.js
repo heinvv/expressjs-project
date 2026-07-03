d45d334f6433acec1f373979dba6294d241e2cfa
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2026-07-03 15:09:27 +0200

    Post and list teams

--- new2026/routes/teams.js
+++ new2026/routes/teams.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const Team = require('../models/team');
 
 router.get('/', async (req, res) => {
-    res.render('teams/index');
+    const teams = await Team.find();
+    res.render( 'teams/index', { teams } );
 });
+
+router.post( '/', async ( req, res ) => {
+    const team = new Team({
+        name: req.body.name
+    });
+
+    try {
+        const newTeam = await team.save();
+        res.redirect( `/teams` );
+    } catch ( err ) {
+        res.render( 'teams/index', {
+            team: team,
+            errorMessage: 'Error creating team'
+        } );
+    }
+});
+
+// edit teams
+
+// delete teams
 
 module.exports = router;
